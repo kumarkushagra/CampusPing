8612748d70fa490f3f6fdd06209c5797ef297875
kumarkushagra <kumarkushagra777@gmail.com>
2024-08-18 23:20:41 +0530

    updates

--- Download_pdf/pdf_to_text_pdf.py
+++ Download_pdf/pdf_to_text_pdf.py
@@ -6,5 +6,4 @@ def pdf_to_text_pdf(pdf_path):
     
 if __name__ == "__main__":
     pdf_path= r"D:\PROJECT\CampusPing\Download_pdf\testing_new.pdf"
-    save_pdf_path = r"D:\PROJECT\CampusPing\Download_pdf\testing_new_OCR.pdf"
     pdf_to_text_pdf(pdf_path)
--- main.py
+++ main.py
@@ -1,1 +1,2 @@
-import fastapi
+def main():
+    1
