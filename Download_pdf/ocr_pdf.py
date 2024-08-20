4f0654c6847e620549d22b516954c8f7f780105f
kumarkushagra <kumarkushagra777@gmail.com>
2024-08-21 02:10:44 +0530

    created requirments.txt and tested it

--- Download_pdf/ocr_pdf.py
+++ Download_pdf/ocr_pdf.py
@@ -1,9 +1,9 @@
 import ocrmypdf
 
-def pdf_to_text_pdf(pdf_path):
+def ocr_pdf(pdf_path):
     ocrmypdf.ocr(pdf_path, pdf_path, force_ocr=True)
     
     
 if __name__ == "__main__":
     pdf_path= r"D:\PROJECT\CampusPing\Download_pdf\testing_new.pdf"
-    pdf_to_text_pdf(pdf_path)
+    ocr_pdf(pdf_path)
--- LLM_api/LLM_contact.py
+++ LLM_api/LLM_contact.py
@@ -1,16 +1,37 @@
+import warnings
+import os
+import sys
 from transformers import pipeline
-
 from huggingface_hub import login
-# Login using your token
-login("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")
+
+# Suppress warnings
+warnings.filterwarnings("ignore")
+
+# Redirect standard output temporarily to suppress login messages
+class HiddenPrints:
+    def __enter__(self):
+        self._original_stdout = sys.stdout
+        self._original_stderr = sys.stderr
+        sys.stdout = open(os.devnull, 'w')
+        sys.stderr = open(os.devnull, 'w')
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        sys.stdout.close()
+        sys.stderr.close()
+        sys.stdout = self._original_stdout
+        sys.stderr = self._original_stderr
+
+with HiddenPrints():
+    # Login using your token
+    login("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")
+
 # Load the summarization model
 summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
+
 # Define a function to summarize the document
 def generate_summary(input_text, max_len=30, min_len=10):
-    summary = summarizer(input_text, max_length=max_len, min_length=min_len, do_sample=False)[0]['summary_text']
+    summary = summarizer(input_text, max_length=max_len, min_length=min_len, do_sample=False, clean_up_tokenization_spaces=True)[0]['summary_text']
     return summary
-
-
 
 # Modify the tags here
 def extract_tags(input_text):
@@ -38,39 +59,21 @@ def extract_tags(input_text):
 def process_notice(input_text):
     tags = extract_tags(input_text)
     summary = generate_summary(input_text)
-    tags_csv = ", ".join(tags)
-    link = "link"  # link add krdiyo tu  
-    file_path = 'latest_notifications.txt'
-
-    # Append the data to the file
-    with open(file_path, 'a') as file:
-        file.write(f"{summary}\n")
-        file.write(f"Link: {link}\n")
-        file.write(f"Tags: {tags_csv}\n")
-        file.write('\n')  # Add a new line between entries
-    
-    return tags_csv, summary
+    return tags, summary
 
 if __name__=="__main__":
     # Example usage
     input_text = """NOTIFICATION
-
-Subject: Mandatory Attendance and Disciplinary Action for Mass Bunking
-
-It is directed to inform you that mass bunking of classes is considered a
-
-serious act of indiscipline. Students found involved in mass bunking will face
-
-strict disciplinary action. Additionally, students involved in such activities will not be granted any relaxation in the attendance criteria in the future, regardless of the circumstances.
-
-Please note that strict action will be taken against first-year students who participate in mass bunking or miss their classes on 21st August 2004 and 27th August 2024. Attendance on these dates is mandatory for all first-year students.
-
-Purthermore, all course instructors teaching first-year students are requested to ensure that attendance for the dates mentioned above is marked in the CUMS system on the same day. This will help identify students who did not attend their classes, and it will be assumed that such students were involved in the mass bunk.
-
-We urge you to comply with these directives and maintain the discipline expected at University.
-          """
+    Subject: Mandatory Attendance and Disciplinary Action for Mass Bunking
+    It is directed to inform you that mass bunking of classes is considered a
+    serious act of indiscipline. Students found involved in mass bunking will face
+    strict disciplinary action. Additionally, students involved in such activities will not be granted any relaxation in the attendance criteria in the future, regardless of the circumstances.
+    Please note that strict action will be taken against first-year students who participate in mass bunking or miss their classes on 21st August 2004 and 27th August 2024. Attendance on these dates is mandatory for all first-year students.
+    Purthermore, all course instructors teaching first-year students are requested to ensure that attendance for the dates mentioned above is marked in the CUMS system on the same day. This will help identify students who did not attend their classes, and it will be assumed that such students were involved in the mass bunk.
+    We urge you to comply with these directives and maintain the discipline expected at University.
+    """
 
     # Get the result
-    tags , summary = process_notice(input_text)
+    tags, summary = process_notice(input_text)
     print(f"tags: {tags}")
     print(f"summary: {summary}")
