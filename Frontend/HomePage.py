376bcbd517f936c83e7364d35df29d827ded9b8f
harshitchopra012 <harshitchopra012@gmail.com>
2024-08-22 22:43:18 +0530

    frontend

--- Frontend/HomePage.py
+++ Frontend/HomePage.py
@@ -1,11 +1,10 @@
 from fastapi import FastAPI, Form, Request
-from fastapi.responses import HTMLResponse
+from fastapi.responses import JSONResponse, HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 import csv
 import os
 import uvicorn
-
 
 app = FastAPI()
 
@@ -19,20 +18,25 @@ async def startup_event():
     if not os.path.exists("student_data.csv"):
         with open("student_data.csv", "w", newline="") as csvfile:
             csvwriter = csv.writer(csvfile)
-            csvwriter.writerow(["Roll Number", "Email ID", "Phone"])
+            csvwriter.writerow(["Roll Number", "Email ID", "Phone", "Tags"])
 
 @app.get("/", response_class=HTMLResponse)
 async def get_form(request: Request):
-    return templates.TemplateResponse("static/test_kushagra.html", {"request": request})
+    return templates.TemplateResponse("test.html", {"request": request})
 
 @app.post("/submit")
-async def submit_form(rollNumber: str = Form(...), email: str = Form(...), phone: str = Form(...)):
+async def submit_form(
+    rollNumber: str = Form(...),
+    email: str = Form(...),
+    phone: str = Form(...),
+    tags: str = Form(...)
+):
     # Append data to the CSV file
     with open("student_data.csv", "a", newline="") as csvfile:
         csvwriter = csv.writer(csvfile)
-        csvwriter.writerow([rollNumber, email, phone])
+        csvwriter.writerow([rollNumber, email, phone, tags])
 
-    return {"message": "Data submitted successfully!"}
+    return JSONResponse(content={"message": "Data submitted successfully!"})
 
 if __name__ == "__main__":
-    uvicorn.run("HomePage:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
