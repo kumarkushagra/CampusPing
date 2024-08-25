0314dd8b9eefc02f219ff3f5af3e6cdf5e949625
kumarkushagra <kumarkushagra777@gmail.com>
2024-08-26 04:29:42 +0530

    Removed build.sh (NO SUDO privilages on railway.app)

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
-#!/bin/bash
-set -e
+# DOES NOT WORK beacuse of NO admin privilages
+# #!/bin/bash 
+# set -e
 
-# Update and install Ghostscript and Tesseract
-sudo apt-get update
-sudo apt-get install -y ghostscript tesseract-ocr tesseract-ocr-eng
+# # Update and install Ghostscript and Tesseract
+# sudo apt-get update
+# sudo apt-get install -y ghostscript tesseract-ocr tesseract-ocr-eng
