62c32de5e0c2b0074fd3a4dbc1e0cb77a548768b
kumarkushagra <kumarkushagra777@gmail.com>
2024-08-26 22:51:28 +0530

    Removed build.sh

--- build.sh
+++
@@ -1,7 +0,0 @@
-# DOES NOT WORK beacuse of NO admin privilages
-# #!/bin/bash 
-# set -e
-
-# # Update and install Ghostscript and Tesseract
-# sudo apt-get update
-# sudo apt-get install -y ghostscript tesseract-ocr tesseract-ocr-eng
